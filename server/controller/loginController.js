c7e6238e9d5823acb87e78bf7ce267110446f965
Nikhil5858 <123rathodnikhil@gmail.com>
2025-06-04 22:45:15 +0530

    login Module Done

---
+++ server/controller/loginController.js
@@ -0,0 +1,52 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Check if user is verified
+    if (!user.isVerified) {
+      return res.status(401).json({ error: 'Email not verified. Please verify using OTP.' });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Generate jwt
+    const token = jwt.sign(
+      { userId: user._id, email: user.email, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1d' }
+    );
+
+    // Send response
+    res.status(200).json({
+      message: 'Login successful',
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      },
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  loginUser
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { startRegistration , verifyOTPAndRegister , resendOTP } = require('../controller/userController');
+const loginController = require('../controller/loginController')
 
 router.post('/register/start', startRegistration);
 router.post('/register/verify', verifyOTPAndRegister);
 router.post('/register/resend-otp', resendOTP);
+router.post('/login',loginController.loginUser)
 
 module.exports = router;
